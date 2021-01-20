8ebd0ea33c4aa3665e1a955b365e2b3881655377
Saadat619 <md.samin.yasar.saadat@g.bracu.ac.bd>
2021-01-20 17:41:56 +0600

    dm for object

--- dynamicMemory.cpp
+++ dynamicMemory.cpp
@@ -1,13 +1,26 @@
 #include<iostream>
 using namespace std;
 
+class Box{
+    public:
+      Box(){
+          cout<< "Constructor called" <<endl;
+      }
+      ~Box(){
+          cout<< "Destructor called" <<endl;
+      }
+};
+
 int main(){
-    double* pvalue = NULL; //pointer initiallize with null
-    pvalue = new double;   //Request memory for the variable
+    // double* pvalue = NULL; //pointer initiallize with null
+    // pvalue = new double;   //Request memory for the variable
 
-    *pvalue = 4564.16;    //Store value at allocated memory
-    cout<< "Value of the pvalue :" <<endl; 
+    // *pvalue = 4564.16;    //Store value at allocated memory
+    // cout<< "Value of the pvalue :" <<endl; 
 
-    delete pvalue;
+    // delete pvalue;  //free up the memory 
+
+    Box* myBoxArray = new Box[4];
+    delete [] myBoxArray; //delete array
     return 0;
 }
