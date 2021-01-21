68bfa04234916b0669b3d5ca3286e3b1922bc645
Saadat619 <md.samin.yasar.saadat@g.bracu.ac.bd>
2021-01-21 14:38:15 +0600

    function like Macro done

---
+++ defineProccessor.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+// #define COUNTRY "Bangladesh"
+#define ONE 1
+#define TWO ONE + ONE
+#define THREE TWO + ONE
+using namespace std;
+
+int main(){
+    // cout << "I am living in " << COUNTRY <<endl;
+    cout << THREE <<endl;
+    return 0;
+}
---
+++ functionlikeMacro.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#define ABS(number) ((number) < 0 ? - (number) : (number))
+using namespace std;
+
+int main(){
+    cout << ABS(-23);
+    return 0;
+}
