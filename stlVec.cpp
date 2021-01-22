d8b8f0b50d392d939fcd8a01e1fdc3d8c7bf00f5
Saadat619 <md.samin.yasar.saadat@g.bracu.ac.bd>
2021-01-22 18:15:32 +0600

    vector basic done

---
+++ stlVec.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+     
+     int array[3];
+
+     vector <int> vec;
+    vec.push_back(35); // vec[0] = 35
+    vec.push_back(15); //vec[1] = 15
+    // 35 15
+    vec.push_back(25); //vec[2] = 25
+    // 35 15 25
+
+    cout << vec[0] <<endl;
+    return 0;
+}
