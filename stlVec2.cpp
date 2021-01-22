8f2ad55fda5c51768d746660dc4fce5de5ae7337
Saadat619 <md.samin.yasar.saadat@g.bracu.ac.bd>
2021-01-22 18:28:56 +0600

    vector size print

---
+++ stlVec2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    //how to print the vector
+    vector<int> vec;
+    vec.push_back(21);
+    vec.push_back(25);
+
+    cout << vec.size() <<endl;
+    
+
+    return 0;
+}
