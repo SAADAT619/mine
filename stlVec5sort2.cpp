751c74d89c16ca52ce57375234f09f8a5db575f9
Saadat619 <md.samin.yasar.saadat@g.bracu.ac.bd>
2021-01-23 14:58:44 +0600

    sorting done

--- stlVec5sort2.cpp
+++ stlVec5sort2.cpp
@@ -5,7 +5,10 @@ using namespace std;
 int main(){
     int ar[5] = {5,3,2,6,8};
 
-    sort(ar, ar+5); //it will sort to index 4 not 5
+    //sort(ar, ar+5); //it will sort to index 4 not 5 mane sob somoe ager index porjonto sort korbe
+
+    //if I want to start sorting from number 2 index
+    sort(ar+2, ar+5);
     for (int i = 0; i < 5; i++)
     {
         cout << ar[i] <<endl;
