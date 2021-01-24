c88d0429d2270f766503eb53cb49a1ed40befbc5
Saadat619 <md.samin.yasar.saadat@g.bracu.ac.bd>
2021-01-24 14:35:50 +0600

    list find first

--- stl8List3.cpp
+++ stl8List3.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 #include<list>
 #include<iterator>
+#include<string>
+#include<algorithm>
 using namespace std;
 
 int main(){
@@ -9,15 +11,29 @@ int main(){
     list<int> mylist(ar, ar+5);
     list<int> :: iterator it;
 
-    it = mylist.begin();
+    // it = mylist.begin(); //This will point to the first element of the array
+    // it++;
+
+    
+
+    // //To print the it iterator element
+    // cout << *it << endl;
+
+    //Use of find function
+    it = find(mylist.begin(), mylist.end(), 6); //Here find function will start from mylist first element and it will search til the element of 6
+    
+    //print this find function
     cout << *it << endl;
 
-    // for (it  = mylist.begin(); it != mylist.end(); it++)
-    // {
-    //     cout << *it << "\t";
-        
-    // }
-    // cout << endl;
+    //To insert any element in the array
+    mylist.insert(it, 7);
+
+    //This loop for print the list
+    for (it  = mylist.begin(); it != mylist.end(); it++)
+    {
+        cout << *it << "\t";
+    }
+    cout << endl;
     
     return 0;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,5 +1,1 @@
-mylist.push_back(10);
-    //  mylist.push_front(15);
-    //  mylist.push_back(1);
-    //  mylist.push_front(13);
-    //  mylist.push_back(-1);
+mylist.insert(it, 7);
