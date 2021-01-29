a1040f7941b508414fe58669e5a192896d103f76
Saadat619 <md.samin.yasar.saadat@g.bracu.ac.bd>
2021-01-29 17:13:43 +0600

    EOF done

---
+++ bracCp3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // int times;
+    // cin >> times;
+
+    int sum=0, num;
+
+    for (int i = 0; i <= times; i++)
+    {
+        cin >> num;
+        sum = sum + num;   
+    }
+    
+    cout << sum << endl;
+ 
+    return 0; 
+} 
