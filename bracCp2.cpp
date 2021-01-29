fcfb7d8d515f998364e3966a064bae7f7e839da8
Saadat619 <md.samin.yasar.saadat@g.bracu.ac.bd>
2021-01-29 16:55:57 +0600

    getline done

---
+++ bracCp2.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+   int n;
+   cin >> n;
+
+   string str;
+   cin.ignore();
+   getline(cin, str); 
+    cout << str << endl; 
+   for (int i = 0; i < n; i++)
+   {
+       cout << str << endl;
+   }
+    
+    
+    return 0;
+} 
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,15 +1,5 @@
-priority_queue<int> q; //This queue is for int values
-
-    q.push(1400);
-    q.push(100);
-    q.push(12);
-    q.push(1700);
-
-
-    //To print the prioeity queue
-    while(!q.empty()){
-        int x;
-        x = q.top();
-        cout << x << endl;
-        q.pop();
-    }
+ for (int i = 0; i < n; i++)
+//    {
+//        cout << str << endl;
+//    }
+   
